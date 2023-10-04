1712ea9f2c24a0299a561469667ea21c184679af
Miguel Ascanio Gomez <mascanio13@gmail.com>
2023-10-04 13:18:24 +0200

    wip2

--- regex-named.go
+++ regex-named.go
@@ -8,15 +8,15 @@ import (
 )
 
 type RegexNamed struct {
-	named_map map[string]int
+	namedMap map[string]int
 	*regexp.Regexp
 }
 
-const error_string = string(unicode.ReplacementChar)
+const errorString = string(unicode.ReplacementChar)
 
 func parseBytes(in []byte, until rune) ([]string, error) {
-	re_named_match := regexp.MustCompile(`^\?P\<(.*?)\>`)
-	re_no_capture_match := regexp.MustCompile(`^\?:`)
+	reNamedMatch := regexp.MustCompile(`^\?P\<(.*?)\>`)
+	reNoCaptureMatch := regexp.MustCompile(`^\?:`)
 	if len(in) == 0 {
 		return nil, nil
 	}
@@ -25,7 +25,7 @@ func parseBytes(in []byte, until rune) ([]string, error) {
 	if nextrun == until {
 		return parseBytes(in, 0)
 	}
-	var prepend_return []string
+	var prependReturn []string
 	switch nextrun {
 	case '\\':
 		if len(in) == 0 {
@@ -39,20 +39,20 @@ func parseBytes(in []byte, until rune) ([]string, error) {
 		}
 		return parseBytes(in, 0)
 	case '(':
-		until_recursive := ')'
-		if m := re_named_match.FindSubmatchIndex(in); m != nil {
+		untilRecursive := ')'
+		if m := reNamedMatch.FindSubmatchIndex(in); m != nil {
 			// Named pattern ?P<name>, return name
-			prepend_return = []string{string(in[m[2]:m[3]])}
+			prependReturn = []string{string(in[m[2]:m[3]])}
 			// skip ?p<name>
 			in = in[m[3]+1:]
-		} else if m := re_no_capture_match.FindSubmatchIndex(in); m != nil {
+		} else if m := reNoCaptureMatch.FindSubmatchIndex(in); m != nil {
 			// no capture, skip ?: part
 			in = in[m[1]+1:]
 		} else {
-			prepend_return = []string{error_string}
+			prependReturn = []string{errorString}
 		}
-		if rec, err := parseBytes(in, until_recursive); err == nil {
-			return append(prepend_return, rec...), nil
+		if rec, err := parseBytes(in, untilRecursive); err == nil {
+			return append(prependReturn, rec...), nil
 		} else {
 			return nil, err
 		}
@@ -68,7 +68,7 @@ func parse(in string) ([]string, error) {
 func buildMap(namedMatches []string) map[string]int {
 	r := make(map[string]int)
 	for i, name := range namedMatches {
-		if name != error_string {
+		if name != errorString {
 			r[name] = i + 1
 		}
 	}
@@ -76,12 +76,12 @@ func buildMap(namedMatches []string) map[string]int {
 }
 
 func Compile(re string) (RegexNamed, error) {
-	compiled_re, err := regexp.Compile(re)
+	compiledRe, err := regexp.Compile(re)
 	if err != nil {
 		return RegexNamed{nil, nil}, err
 	}
 	if parsed, err := parse(re); err == nil {
-		return RegexNamed{buildMap(parsed), compiled_re}, nil
+		return RegexNamed{buildMap(parsed), compiledRe}, nil
 	} else {
 		return RegexNamed{nil, nil}, err
 	}
@@ -95,11 +95,64 @@ func MustCompile(re string) RegexNamed {
 	return r
 }
 
-func (re *RegexNamed) FindStringNamed(s, name string) string {
-	idx, ok := re.named_map[name]
-	if !ok {
-		return ""
+func composeMap[T any](match []T, pos int) T {
+	return match[pos]
+}
+
+func composeIndex[T any](match []T, pos int) []T {
+	return match[pos*2 : pos*2+2]
+}
+
+func mapRe[T, S any](re *RegexNamed, match []T, f func([]T, int) S) (S, map[string]S) {
+	if match == nil {
+		return *new(S), nil
 	}
-	match := re.FindStringSubmatch(s)
-	return match[idx]
+	rv := make(map[string]S)
+	for k, v := range re.namedMap {
+		rv[k] = f(match, v)
+	}
+	return f(match, 0), rv
+}
+
+func mapReAll[T, S any](re *RegexNamed, match [][]T, f func([]T, int) S) ([]S, []map[string]S) {
+	rv := make([]map[string]S, 0)
+	rv0 := make([]S, 0)
+	for _, m := range match {
+		base, mm := mapRe(re, m, f)
+		rv0 = append(rv0, base)
+		rv = append(rv, mm)
+	}
+	return rv0, rv
+}
+
+func (re *RegexNamed) FindNamed(s []byte) ([]byte, map[string][]byte) {
+	return mapRe(re, re.FindSubmatch(s), composeMap)
+}
+
+func (re *RegexNamed) FindIndexNamed(s []byte) ([]int, map[string][]int) {
+	return mapRe(re, re.FindSubmatchIndex(s), composeIndex)
+}
+
+func (re *RegexNamed) FindStringNamed(s string) (string, map[string]string) {
+	return mapRe(re, re.FindStringSubmatch(s), composeMap)
+}
+
+func (re *RegexNamed) FindStringIndexNamed(s string) ([]int, map[string][]int) {
+	return mapRe(re, re.FindStringSubmatchIndex(s), composeIndex)
+}
+
+func (re *RegexNamed) FindAllNamed(s []byte, n int) ([][]byte, []map[string][]byte) {
+	return mapReAll(re, re.FindAllSubmatch(s, n), composeMap)
+}
+
+func (re *RegexNamed) FindAllIndexNamed(s []byte, n int) ([][]int, []map[string][]int) {
+	return mapReAll(re, re.FindAllSubmatchIndex(s, n), composeIndex)
+}
+
+func (re *RegexNamed) FindAllStringNamed(s string, n int) ([]string, []map[string]string) {
+	return mapReAll(re, re.FindAllStringSubmatch(s, n), composeMap)
+}
+
+func (re *RegexNamed) FindAllStringIndexNamed(s string, n int) ([][]int, []map[string][]int) {
+	return mapReAll(re, re.FindAllStringSubmatchIndex(s, n), composeIndex)
 }
--- regex-named_test.go
+++ regex-named_test.go
@@ -6,16 +6,191 @@ import (
 
 func TestFindStringNamed(t *testing.T) {
 	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
-	m := re.FindStringNamed("foo 42", "name")
-	if m != "foo" {
-		t.Errorf("Expected 'foo', got '%s'", m)
+	if m0, m := re.FindStringNamed("foo 42"); m == nil {
+		t.Error("Expected match")
+	} else {
+		if m0 != "foo 42" {
+			t.Error("Expected match to be foo 42")
+		}
+		if m["name"] != "foo" {
+			t.Error("Expected name to be foo")
+		}
+		if m["age"] != "42" {
+			t.Error("Expected age to be 42")
+		}
 	}
-	m = re.FindStringNamed("foo 42", "age")
-	if m != "42" {
-		t.Errorf("Expected '42', got '%s'", m)
+}
+
+func TestFindAllStringNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+)? (?P<age>\d+)`)
+	if m0, m := re.FindAllStringNamed("foo 42 43", -1); m == nil {
+		t.Error("Expected match")
+	} else {
+		if m0[0] != "foo 42" {
+			t.Error("Expected match to be foo 42")
+		}
+		if m[0]["name"] != "foo" {
+			t.Error("Expected name to be foo")
+		}
+		if m[0]["age"] != "42" {
+			t.Error("Expected age to be 42")
+		}
+		if m0[1] != " 43" {
+			t.Error("Expected match to be  43")
+		}
+		if m[1]["name"] != "" {
+			t.Error("Expected name to be bar")
+		}
+		if m[1]["age"] != "43" {
+			t.Error("Expected age to be 43")
+		}
 	}
-	m = re.FindStringNamed("foo 42", "foo")
-	if m != "" {
-		t.Errorf("Expected '', got '%s'", m)
+}
+
+func TestFindNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindNamed([]byte("foo 42")); m == nil {
+		t.Error("Expected match")
+	} else {
+		if string(m0) != "foo 42" {
+			t.Error("Expected match to be foo 42")
+		}
+		if string(m["name"]) != "foo" {
+			t.Error("Expected name to be foo")
+		}
+		if string(m["age"]) != "42" {
+			t.Error("Expected age to be 42")
+		}
+	}
+}
+
+func TestFindAllNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindAllNamed([]byte("foo 42 bar 43"), -1); m == nil {
+		t.Error("Expected match")
+	} else {
+		if string(m0[0]) != "foo 42" {
+			t.Error("Expected match to be foo 42")
+		}
+		if string(m[0]["name"]) != "foo" {
+			t.Error("Expected name to be foo")
+		}
+		if string(m[0]["age"]) != "42" {
+			t.Error("Expected age to be 42")
+		}
+		if string(m0[1]) != "bar 43" {
+			t.Error("Expected match to be bar 43")
+		}
+		if string(m[1]["name"]) != "bar" {
+			t.Error("Expected name to be bar")
+		}
+		if string(m[1]["age"]) != "43" {
+			t.Error("Expected age to be 43")
+		}
+	}
+}
+
+func sliceEq[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestFindIndexNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindIndexNamed([]byte("foo 42")); m == nil {
+		t.Error("Expected match")
+	} else {
+		if !sliceEq(m0, []int{0, 6}) {
+			t.Error("Expected match to be {0, 6}")
+		}
+		if !sliceEq(m["name"], []int{0, 3}) {
+			t.Error("Expected name to be {0, 3}")
+		}
+		if !sliceEq(m["age"], []int{4, 6}) {
+			t.Error("Expected age to be {4, 6}")
+		}
+	}
+}
+
+func TestFindStringIndexNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindStringIndexNamed("foo 42"); m == nil {
+		t.Error("Expected match")
+	} else {
+		if !sliceEq(m0, []int{0, 6}) {
+			t.Error("Expected match to be {0, 6}")
+		}
+		if !sliceEq(m["name"], []int{0, 3}) {
+			t.Error("Expected name to be {0, 3}")
+		}
+		if !sliceEq(m["age"], []int{4, 6}) {
+			t.Error("Expected age to be {4, 6}")
+		}
+	}
+}
+
+func TestFindAllIndexNamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindAllIndexNamed([]byte("foo 42 bar 43"), -1); m == nil {
+		t.Error("Expected match")
+	} else {
+		if !sliceEq(m0[0], []int{0, 6}) {
+			t.Error("Expected match to be {0, 6}")
+		}
+		if !sliceEq(m[0]["name"], []int{0, 3}) {
+			t.Error("Expected name to be {0, 3}")
+		}
+		if !sliceEq(m[0]["age"], []int{4, 6}) {
+			t.Error("Expected age to be {4, 6}")
+		}
+		if !sliceEq(m0[1], []int{7, 13}) {
+			t.Error("Expected match to be {7, 13}")
+		}
+		if !sliceEq(m[1]["name"], []int{7, 10}) {
+			t.Error("Expected name to be {7, 10}")
+		}
+		if !sliceEq(m[1]["age"], []int{11, 13}) {
+			t.Error("Expected age to be {11, 13}")
+		}
+	}
+}
+
+func TestFindAllStringIndexnamed(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if m0, m := re.FindAllStringIndexNamed("foo 42 bar 43", -1); m == nil {
+		t.Error("Expected match")
+	} else {
+		if !sliceEq(m0[0], []int{0, 6}) {
+			t.Error("Expected match to be {0, 6}")
+		}
+		if !sliceEq(m[0]["name"], []int{0, 3}) {
+			t.Error("Expected name to be {0, 3}")
+		}
+		if !sliceEq(m[0]["age"], []int{4, 6}) {
+			t.Error("Expected age to be {4, 6}")
+		}
+		if !sliceEq(m0[1], []int{7, 13}) {
+			t.Error("Expected match to be {7, 13}")
+		}
+		if !sliceEq(m[1]["name"], []int{7, 10}) {
+			t.Error("Expected name to be {7, 10}")
+		}
+		if !sliceEq(m[1]["age"], []int{11, 13}) {
+			t.Error("Expected age to be {11, 13}")
+		}
+	}
+}
+
+func TestSubexpNames(t *testing.T) {
+	re := MustCompile(`(?P<name>\w+) (?P<age>\d+)`)
+	if sliceEq(re.SubexpNames(), []string{"name", "age"}) {
+		t.Error("Expected SubexpNames to be {name, age}")
 	}
 }
